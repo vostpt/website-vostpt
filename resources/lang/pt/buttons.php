def84fe1028048bc910f214666868f4583df4f29
Ivo Barros <myL3v3L@gmail.com>
2019-04-10 02:18:40 +0100

    feat(locale): add new locale file with button and link text

---
+++ resources/lang/pt/buttons.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Button and link Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'who_are_we' => 'Quem somos',
+    'mission' => 'Missão',
+    'objectives' => 'Objectivos',
+    'blog' => 'Blog',
+    'more' => 'Mais',
+    'vost_feed' => 'Vost feed',
+    'contact' => 'Contacto',
+    'media' => 'Media',
+    'privacy_policy' => 'Política de Privacidade',
+];
