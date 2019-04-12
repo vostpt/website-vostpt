8fc81022df290a9d726046301041452e4fcc739b
Ivo Barros <myL3v3L@gmail.com>
2019-04-12 23:46:33 +0100

    feat(config): add social links to configuration

---
+++ config/social.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'facebook' => 'https://www.facebook.com/Vostpt/',
+    'twitter' => 'https://twitter.com/vostpt',
+    'instagram' => 'https://www.instagram.com/vostpt/',
+    'reddit' => 'https://reddit.com/u/vostpt',
+    'discord' => 'https://discord.gg/ScvE5tR'
+];
