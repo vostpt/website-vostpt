7af4456c2ec4000aae9d998abc1d7795c0c452ef
Ivo Barros <myL3v3L@gmail.com>
2019-04-15 17:42:22 +0100

    fix(navbar): change default navbar to dark

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky-top">
+<nav class="navbar navbar-expand-md navbar-dark navbar-laravel sticky-top">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
