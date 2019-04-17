3d1779d04edc118977b5b937245b9fdf36f019dd
Ivo Barros <myL3v3L@gmail.com>
2019-04-17 19:20:49 +0100

    feat(controllers): create controllers for each page with hero data

---
+++ app/Http/Controllers/MissionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MissionController extends Controller
+{
+    public function index() {
+        return view('mission', ['title' =>  __('strings.missao_titulo1'), 'subtitle' => __('strings.missao_subtitulo1'), 'image' => asset('storage/VOSTSITE_0052.jpg')]);
+    }
+}
