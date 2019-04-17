f510c83e7df51a6d29d3579b17ea5cd112664589
Ivo Barros <myL3v3L@gmail.com>
2019-04-17 14:44:58 +0100

    feat(components): add new hero component

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,6 @@
+<div class="hero full-height flex-center position-ref" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ $image }}');">
+        <div class="container">
+          <h1 class="display-4">{{$title}}</h1>
+          <p class="lead">{{$subtitle}}</p>
+        </div>
+</div>
