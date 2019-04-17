dc11f7ed9f879f4b0b35f7c9ba8342c78e172911
Ivo Barros <myL3v3L@gmail.com>
2019-04-17 15:04:09 +0100

    refact(navbar js): move code into specific file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -55,36 +55,3 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
-
-//switch navbar theme depending on scroll position
-$(window).scroll(function() {
-    let scrollPos = $(window).scrollTop();
-
-    if (scrollPos >= 1) {
-        $(".navbar-laravel").removeClass("navbar-dark");
-        $(".navbar-laravel").addClass("navbar-light");
-    } else {
-        if (!$(".navbar-collapse.show").length) {
-            $(".navbar-laravel").removeClass("navbar-light");
-            $(".navbar-laravel").addClass("navbar-dark");
-        }
-    }
-});
-
-//handle mobile collapible colors
-$(".navbar-laravel").on("show.bs.collapse", function(e) {
-    $(".navbar-laravel").removeClass("navbar-dark");
-    $(".navbar-laravel").addClass("navbar-light");
-});
-
-$(".navbar-dark").on("hide.bs.collapse", function(e) {
-    let scrollPos = $(window).scrollTop();
-
-    if (scrollPos >= 1) {
-        $(".navbar-laravel").removeClass("navbar-dark");
-        $(".navbar-laravel").addClass("navbar-light");
-    } else {
-        $(".navbar-laravel").removeClass("navbar-light");
-        $(".navbar-laravel").addClass("navbar-dark");
-    }
-});
