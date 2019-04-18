4ff827968a2120a8aa4766d2aebd09521277f84c
Ivo Barros <myL3v3L@gmail.com>
2019-04-18 17:27:58 +0100

    feat(components): add icon card component
    fixes #26

---
+++ resources/views/components/iconCard.blade.php
@@ -0,0 +1,7 @@
+<div class="text-center px-lg-3">
+    <span class="btn btn-icon btn-lg btn-soft-success rounded-circle mb-5">
+        <span class="{{$fontClass}} fa-2x btn-icon__inner btn-icon__inner-bottom-minus"></span>
+    </span>
+    <h3 class="h5">{{$title}}</h3>
+    <p class="mb-md-0">{{$body}}</p>
+</div>
