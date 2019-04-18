06262a67b48bab2dc2fe38e2d2b4dd1456e2bf01
Ivo Barros <myL3v3L@gmail.com>
2019-04-18 12:31:37 +0100

    fix(webpack): set public path and resource path

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.setPublicPath('public');
+mix.setResourceRoot('../');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
