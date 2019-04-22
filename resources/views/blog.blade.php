47cf0055bdf99988953c6d3c6f48815580a631d5
Ivo Barros <myL3v3L@gmail.com>
2019-04-22 22:45:47 +0100

    fix(* pages): replace content class on body with container class

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="main__body main__body--raised flex-center position-ref">
-        <div class="content">
+        <div class="container">
             <div class="title marigin-bottom-md">
                 {{ config('app.name', 'Laravel') }}
             </div>
