167ab5f7b3cd5ca77241bc345056ff97075c1c11
Ivo Barros <myL3v3L@gmail.com>
2019-05-02 23:47:47 +0100

    feat(whoweare page): add member image card component

--- resources/views/who_are_we.blade.php
+++ resources/views/who_are_we.blade.php
@@ -10,9 +10,12 @@
         ]
     ])
 
-    @include('components.empty',[
-        'sectionTitle' => __('strings.somos_titulo3')
+    @include('components.imageCard', [
+        'sectionTitle' => __('strings.somos_titulo3'),
+        'columnMax' => 4,
+        'cards' => $arrayMembers
     ])
+
     @include('components.empty',[
         'sectionTitle' => __('strings.somos_titulo4'),
         'sectionSubtitle' => __('strings.somos_subtitulo2')
