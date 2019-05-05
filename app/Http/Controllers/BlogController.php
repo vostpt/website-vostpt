717e07db2e4caf4871f2dca95c5055e5f19800f6
Ivo Barros <myL3v3L@gmail.com>
2019-05-05 05:20:36 +0100

    feat(blog index): moved to folder, added card deck component with posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class BlogController extends Controller
 {
     public function index() {
-        return view('blog', ['title' =>  __('strings.home_titulo1'), 'subtitle' => __('strings.home_subtitulo1'), 'image' => asset('storage/VOSTSITE_0053.jpg')]);
+        $posts = Post::all();
+        $posts = $posts->map(function ($item) {
+            $item->url = route('blog.show', ['blog' => $item]);
+            return $item;
+        });
+        return view('blog.index', ['title' =>  __('strings.home_titulo1'), 'subtitle' => __('strings.home_subtitulo1'), 'image' => asset('storage/VOSTSITE_0053.jpg'), 'posts' => $posts]);
     }
 }
