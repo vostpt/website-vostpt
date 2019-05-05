d1d7b4674c3ea2ae652686a5c6359bf1a5fb65f2
Ivo Barros <myL3v3L@gmail.com>
2019-05-05 05:16:31 +0100

    feat(components): add card deck component

---
+++ resources/views/components/cardDeck.blade.php
@@ -0,0 +1,25 @@
+@extends('components.section')
+
+@section('sectionContent')
+
+@for ($i = 0; $i < ceil(count($cards)/$columnMax); $i++)
+    <div class="card-deck cardDeck">
+        @for ($j = $i; $j < ($i + $columnMax); $j++)
+            @isset($cards[$j])
+                <div class="card">
+                    <div class="card-img-top rounded">
+                            <a href="{{$cards[$j]->url}}">
+                                <img src="{{$cards[$j]->cover}}" class="" alt="...">
+                            </a>
+                    </div>
+                    <div class="card-body">
+                            <a href="{{$cards[$j]->url}}"><h5 class="card-title">{{$cards[$j]->title}}</h5></a>
+                        <p class="card-text">{{Str::limit($cards[$j]->body,200)}}</p>
+                    </div>
+                </div>
+            @endisset
+        @endfor
+    </div>
+@endfor
+
+@overwrite
