cc9a28defb683f048c05455d27189ac8e96a91ff
Ivo Barros <myL3v3L@gmail.com>
2019-05-23 13:40:30 +0100

    feat(Social links): add telegram link

--- config/social.php
+++ config/social.php
@@ -6,6 +6,7 @@ return [
     'instagram' => 'https://www.instagram.com/vostpt/',
     'reddit' => 'https://reddit.com/u/vostpt',
     'discord' => 'https://discord.gg/ScvE5tR',
+    'telegram' => 'https://t.me/vostportugal',
     'websiteTeam' => 'https://github.com/vostpt/website-vostpt/graphs/contributors',
     'rss' => env('RSS')
 ];
