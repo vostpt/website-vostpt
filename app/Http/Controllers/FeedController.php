222b8ee0955b78bf4c3b6a84dc302b23a51c435a
Ivo Barros <myL3v3L@gmail.com>
2019-06-01 01:07:29 +0100

    fix(feed page): replace text component with card deck component

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -9,12 +9,15 @@ use Carbon\Carbon;
 class FeedController extends Controller
 {
     public function index() {
-        $socialPosts = SocialPost::orderBy('pubDate','desc')->get();
+        $socialPosts = SocialPost::orderBy('pubDate','desc')->limit(6)->get();
         $socialPosts = $socialPosts->map(function ($item) {
-            return [
-                'title' => $item->platform." - ".Carbon::parse($item->pubDate)->diffForHumans(),
-                'description' => $item->text
-            ];
+            $item->title = $item->platform." - ".Carbon::parse($item->pubDate)->diffForHumans();
+            $item->body = $item->text;
+            if(!empty($item->media)){
+                $item->cover = $item->media;
+            }
+            $item->url = $item->socialUrl;
+            return $item;
         });
         return view('vost_feed', ['title' =>  __('strings.feed_titulo1'), 'subtitle' => __('strings.feed_subtitulo1'), 'image' => asset('storage/VOSTSITE_0053.jpg'), 'socialPosts' => $socialPosts]);
     }
