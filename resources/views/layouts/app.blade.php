981c2f06bf088e2b8a5423cf33c74d4b7fc7aec2
Ivo Barros <myL3v3L@gmail.com>
2019-06-03 23:30:46 +0100

    chore(app layout): extract head into partial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('storage/favicon-32x32.png') }}">
-</head>
+@include('partials.head')
 <body>
     @include('partials.navbar')
     @include('partials.hero', ['title' =>  $title, 'subtitle' => $subtitle, 'image' => $image, 'halfHeight' => (isset($halfHeight) && $halfHeight?$halfHeight:null)])
