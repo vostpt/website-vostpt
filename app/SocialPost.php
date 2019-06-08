9f421435a80c10251682000eda00160a2564350d
Ivo Barros <myL3v3L@gmail.com>
2019-06-08 07:10:12 +0100

    feat(SocialPost): use media library package to save social media objects

--- app/SocialPost.php
+++ app/SocialPost.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class SocialPost extends Model
+class SocialPost extends Model implements HasMedia
 {
+    use HasMediaTrait;
     protected $fillable = ['socialId'];
 }
