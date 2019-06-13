8f32b1186299a5ee9ac769d3bb4c05cd11c3944c
Ivo Barros <myL3v3L@gmail.com>
2019-06-13 21:38:15 +0100

    feat(app): add contact form partial

---
+++ resources/views/partials/contactForm.blade.php
@@ -0,0 +1,29 @@
+<div class="contactForm">
+    <div class="contactForm__title">
+        Entre em contacto
+    </div>
+    <form>
+        <div class="form-group">
+            <input class="form-control" type="text" placeholder="O seu nome">
+        </div>
+
+        <div class="form-group">
+            <input type="email" class="form-control" aria-describedby="email" placeholder="O seu email">
+        </div>
+
+        <div class="form-group">
+            <input class="form-control" type="text" placeholder="O seu assunto">
+        </div>
+
+
+        <div class="form-group">
+            <textarea class="form-control" rows="8" placeholder="A sua mensagem"></textarea>
+        </div>
+
+        <div>
+            CAPTCHA
+        </div>
+
+        <button type="submit" class="btn btn-danger">ENVIAR MENSAGEM</button>
+    </form>
+</div>
