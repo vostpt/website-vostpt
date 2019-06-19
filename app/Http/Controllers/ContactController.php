c4af9e9f20d3ab5d9fba28bc5f7e9f95e5531da9
Ivo Barros <myL3v3L@gmail.com>
2019-06-19 21:44:50 +0100

    feat(Contact Control): add store function

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
     public function index() {
         return view('contact', ['title' =>  __('strings.contacto_titulo1'), 'subtitle' => __('strings.contacto_subtitulo1'), 'image' => asset('storage/VOSTSITE_0053.jpg')]);
+    }
+
+    public function store(Request $request) {
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+            'g-recaptcha-response' => 'required|captcha'
+        ]);
+
+        $data = $request->all();
+        $check = true;
+        //$check = Messages::insert($data);
+        $arr = array('msg' => 'Something went wrong. Please try again later', 'status' => false);
+        if($check){
+        $arr = array('msg' => 'Successfully submitted form using ajax', 'status' => true);
+        }
+        return Response()->json($arr);
     }
 }
