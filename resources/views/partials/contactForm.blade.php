d4d802da17d1330bd8fb30f46fa8b97051add4dc
Ivo Barros <myL3v3L@gmail.com>
2019-06-19 21:46:46 +0100

    feat(Contact form partial): add front end functionality

--- resources/views/partials/contactForm.blade.php
+++ resources/views/partials/contactForm.blade.php
@@ -1,32 +1,115 @@
 <div class="contactForm float-right">
     <div class="contactForm__title">
-        Entre em contacto
+        @lang('strings.contacto_form_titulo')
     </div>
-    <form>
+
+    <div class="sent-message">
+        <h3>Sent</h3>
+    </div>
+
+    <form id="contact_us" class='is-sent' action="">
+        @csrf
+
         <div class="form-group">
-            <input class="form-control" type="text" placeholder="O seu nome">
+            <input type="text" name="name" class="form-control"  placeholder="@lang('strings.contacto_form_nome')"/>
         </div>
 
         <div class="form-group">
-            <input type="email" class="form-control" aria-describedby="email" placeholder="O seu email">
+            <input type="email" name="email" class="form-control"  placeholder="@lang('strings.contacto_form_email')"/>
         </div>
 
         <div class="form-group">
-            <input class="form-control" type="text" placeholder="O seu assunto">
+            <input type="text" name="subject" class="form-control"  placeholder="@lang('strings.contacto_form_assunto')"/>
         </div>
-
 
         <div class="form-group">
-            <textarea class="form-control" rows="8" placeholder="A sua mensagem"></textarea>
+            <textarea type="text" name="message" class="form-control" rows="8" placeholder="@lang('strings.contacto_form_mensagem')"></textarea>
         </div>
-
-
-        <div class="text-center">
+        <div class="form-group">
             {!! NoCaptcha::renderJs() !!}
             {!! NoCaptcha::display() !!}
-            <button id="my-form-id-submit" type="submit" class="btn btn-danger">ENVIAR MENSAGEM</button>
+        </div>
+
+        <div class="text-center">
+
+            <button type="submit" class="btn btn-danger">@lang('strings.contacto_form_button')</button>
         </div>
 
     </form>
 </div>
+
+<script>
+    window.addEventListener('load', function() {
+        //default validation config
+        jQuery.validator.setDefaults({
+            ignore: [],
+            highlight: function(element) {
+                jQuery(element).closest('.form-control').addClass('is-invalid');
+            },
+            unhighlight: function(element) {
+                jQuery(element).closest('.form-control').removeClass('is-invalid');
+            },
+            errorElement: 'div',
+            errorClass: 'invalid-feedback',
+            errorPlacement: function(error, element) {
+                if(element.parent('.input-group').length) {
+                    error.insertAfter(element.parent());
+                } else {
+                    element.closest('.form-group').append(error);
+                }
+            }
+        });
+
+        var formElement = $('#contact_us');
+
+        //Validation for form
+        var validator = $('#contact_us').validate({
+            rules: {
+                'name': "required",
+                'email': {
+                    'required': true,
+                    'email': true
+                },
+                'message': {
+                    'required': true,
+                    'minlength': 5
+                },
+                'g-recaptcha-response': "required"
+            },
+
+            messages: {
+                'name': "Please enter your name",
+                'message': {
+                    'required': "Please provide a message",
+                    'minlength': "Your message must be at least 5 characters long"
+                },
+                email: "Please enter a valid email address",
+                'g-recaptcha-response': "required"
+            },
+
+            success: "valid",
+            submitHandler: function() { ajaxComplete() }
+        });
+
+        //send data back to server
+        function ajaxComplete() {
+            let formData = new FormData($('#contact_us')[0]);
+
+            axios
+                .post('{{ route("saveForm") }}', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+                .then(function(response) {
+                    formElement.hide();
+                    $('.sent-message').show();
+                })
+                .catch(function (error) {
+                    // handle error
+                    validator.showErrors(error.response.data.errors)
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    })
+
+</script>
 
