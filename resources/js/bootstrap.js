34a90317ed4ae6e639ce3fce470064ce3b4255da
Ivo Barros <myL3v3L@gmail.com>
2019-06-19 21:36:20 +0100

    feat(npm): add jquery validation plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,7 @@ window._ = require("lodash");
 try {
     window.Popper = require("popper.js").default;
     window.$ = window.jQuery = require("jquery");
-
+    require("jquery-validation");
     require("bootstrap");
 } catch (e) {}
 
