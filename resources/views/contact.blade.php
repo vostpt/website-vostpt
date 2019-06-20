3ddaada8a680cc612adb40f8f537939a71b63e8d
Ivo Barros <myL3v3L@gmail.com>
2019-06-20 23:29:20 +0100

    refact(Contact page): move text into locale file
    fixes #72
    fixes #73

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,14 +5,9 @@
     @sectionRow(['paralaxImage' => asset('storage/VOSTSITE_0060.jpg')])
         <div class="col-6">
             <div>
-                <h2>Como nos contactar</h2>
-                <h3>Queres juntar-te a nós?</h3>
-                <p>
-                    Na VOST Portugal estamos sempre à procura de pessoas que se queiram juntar à nossa equipa. Se sentes que podes ajudar de alguma maneira, seja com conteúdos, gestão de redes sociais, validação de informação, meteorologia, ou outros, e se o teu espírito é igual ao nosso seria um prazer ter-te connosco.
-                    <br>Preenche este formulário.
-                </p>
-                <p>Se és developer, e queres ajudar, vai até ao nosso GitHub.</p>
-                <p>Para qualquer outro tipo de contactos preenche por favor o formulário de contacto.</p>
+                <h2>@lang('strings.contacto_titulo2')</h2>
+                <h3>@lang('strings.contacto_titulo3')</h3>
+                @lang('strings.contacto_text1')
             </div>
         </div>
 
