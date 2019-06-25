af2409a61a572541b5dbd9c5e76bebb64086c0e5
Ivo Barros <myL3v3L@gmail.com>
2019-06-25 04:35:47 +0100

    refact(Contact form): extract validation defaults and ajax function

--- resources/views/partials/contactForm.blade.php
+++ resources/views/partials/contactForm.blade.php
@@ -42,78 +42,40 @@
     </form>
 </div>
 
-<script>
-    window.addEventListener('load', function() {
-        //default validation config
-        jQuery.validator.setDefaults({
-            ignore: [],
-            highlight: function(element) {
-                jQuery(element).closest('.form-control').addClass('is-invalid');
-            },
-            unhighlight: function(element) {
-                jQuery(element).closest('.form-control').removeClass('is-invalid');
-            },
-            errorElement: 'div',
-            errorClass: 'invalid-feedback',
-            errorPlacement: function(error, element) {
-                if(element.parent('.input-group').length) {
-                    error.insertAfter(element.parent());
-                } else {
-                    element.closest('.form-group').append(error);
-                }
-            }
-        });
+@push('script-body')
+    <script>
+        window.addEventListener('load', function() {
+            let formElement = $('#contact_us');
 
-        var formElement = $('#contact_us');
-
-        //Validation for form
-        var validator = $('#contact_us').validate({
-            rules: {
-                'name': "required",
-                'email': {
-                    'required': true,
-                    'email': true
+            //Validation for form
+            let validator = formElement.validate({
+                rules: {
+                    'name': "required",
+                    'email': {
+                        'required': true,
+                        'email': true
+                    },
+                    'message': {
+                        'required': true,
+                        'minlength': 5
+                    },
+                    'g-recaptcha-response': "required"
                 },
-                'message': {
-                    'required': true,
-                    'minlength': 5
+
+                messages: {
+                    'name': "Please enter your name",
+                    'message': {
+                        'required': "Please provide a message",
+                        'minlength': "Your message must be at least 5 characters long"
+                    },
+                    email: "Please enter a valid email address",
+                    'g-recaptcha-response': "required"
                 },
-                'g-recaptcha-response': "required"
-            },
 
-            messages: {
-                'name': "Please enter your name",
-                'message': {
-                    'required': "Please provide a message",
-                    'minlength': "Your message must be at least 5 characters long"
-                },
-                email: "Please enter a valid email address",
-                'g-recaptcha-response': "required"
-            },
+                success: "valid",
+                submitHandler: function() { window.ajaxComplete(formElement, '{{ route("saveForm") }}') }
+            });
 
-            success: "valid",
-            submitHandler: function() { ajaxComplete() }
-        });
-
-        //send data back to server
-        function ajaxComplete() {
-            let formData = new FormData($('#contact_us')[0]);
-
-            axios
-                .post('{{ route("saveForm") }}', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-                .then(function(response) {
-                    formElement.hide();
-                    $('.sent-message').show();
-                })
-                .catch(function (error) {
-                    // handle error
-                    validator.showErrors(error.response.data.errors)
-                })
-                .finally(function () {
-                    // always executed
-                });
-        }
-    })
-
-</script>
-
+        })
+    </script>
+@endpush
