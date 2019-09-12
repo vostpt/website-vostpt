0682f71183b0dea49f69ee28893d12b224739b4d
Ivo Barros <myL3v3L@gmail.com>
2019-09-12 03:01:43 +0100

    fix(app service provider): limit string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //blade components
         Blade::component('components.sectionRow', 'sectionRow');
 
